14be3deb804160183b94cf237e027b1bd8139daf
rocas3096 <ozavala3096@gmail.com>
2023-03-17 12:00:37 -0500

    testing if git will save this

--- Develop/script.js
+++ Develop/script.js
@@ -4,7 +4,7 @@
 var uppercase="ABCDEFGHIJKLMNOPQRSTUVWXYZ"
 var lowercase="abcdefghijklmnopqrstuvwxyz"
 var numbers="0123456789"
-var special="~`!@#$%^&*()-_=+[]{}|;\"\':<>,./?"
+var special="~`!@#$%^&*()-_=+[]{}|;\"':<>,./?"
 
 
 
