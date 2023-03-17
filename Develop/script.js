f0d373a6aa2158a2347bb24a7c16ad16da850292
rocas3096 <ozavala3096@gmail.com>
2023-03-17 12:06:02 -0500

    Created function to generate passowrd

--- Develop/script.js
+++ Develop/script.js
@@ -7,6 +7,19 @@ var numbers="0123456789"
 var special="~`!@#$%^&*()-_=+[]{}|;\"':<>,./?"
 
 
+//Write function for generating password
+function generatePassword () {
+  var password=""
+  var passwordlength= prompt("How long would you like your password to be?\n \nChoose a length between 8 and 128 characters")
+
+
+  //Double check for password length
+  if (passwordlength < 8 || passwordlength > 128) {
+    alert("Password length must be between 8 and 128 characters")
+    return generatePassword()
+  }
+}
+
 
 
 // Get references to the #generate element
